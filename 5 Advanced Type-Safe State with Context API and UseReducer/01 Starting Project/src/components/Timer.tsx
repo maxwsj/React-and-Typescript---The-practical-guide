4ead05330bf968906c9f0427b21f3d6688d24d79
maxwsj <mw2study@gmail.com>
2025-04-08 22:07:44 -0300

    advanced type-safe state with context API and useReducer: - create a type-safe provider component - access context type-safe with a custom hook - use useReducer with typescript - create a bsic reducer function and a basic action type - change state via the reducer function - use a better approach for action types

--- 5 Advanced Type-Safe State with Context API and UseReducer/01 Starting Project/src/components/Timer.tsx
+++ 5 Advanced Type-Safe State with Context API and UseReducer/01 Starting Project/src/components/Timer.tsx
@@ -1,9 +1,11 @@
 import Container from './UI/Container.tsx';
+import { Timer as TimeProps } from '../store/timers-context.tsx';
 
-export default function Timer() {
-  return (
-    <Container as="article">
-      <h2>TODO: TIMER NAME</h2>
-    </Container>
-  );
+export default function Timer({ name, duration }: TimeProps) {
+   return (
+      <Container as="article">
+         <h2>{name}</h2>
+         <p>{duration}</p>
+      </Container>
+   );
 }
